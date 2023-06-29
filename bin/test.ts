a1cad8c8f3a973aa44ce0505c380918faf0eca74
Harminder Virk <virk.officials@gmail.com>
2023-06-29 13:33:20 +0530

    chore: upgrade japa to v3

--- bin/test.ts
+++ bin/test.ts
@@ -1,9 +1,5 @@
 import { assert } from '@japa/assert'
-import { pathToFileURL } from 'node:url'
-import { expectTypeOf } from '@japa/expect-type'
-import { specReporter } from '@japa/spec-reporter'
-import { runFailedTests } from '@japa/run-failed-tests'
-import { processCliArgs, configure, run } from '@japa/runner'
+import { processCLIArgs, configure, run } from '@japa/runner'
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +14,10 @@ import { processCliArgs, configure, run } from '@japa/runner'
 |
 | Please consult japa.dev/runner-config for the config docs.
 */
+processCLIArgs(process.argv.slice(2))
 configure({
-  ...processCliArgs(process.argv.slice(2)),
-  ...{
-    files: ['tests/**/*.spec.ts'],
-    plugins: [assert(), runFailedTests(), expectTypeOf()],
-    reporters: [specReporter()],
-    importer: (filePath) => import(pathToFileURL(filePath).href),
-  },
+  files: ['tests/**/*.spec.ts'],
+  plugins: [assert()],
 })
 
 /*
